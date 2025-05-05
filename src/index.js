84a4052c80641ef1ee8b49b3a565cf849e45420d
Danilo Steps <45054191+DaniloSteps@users.noreply.github.com>
2025-05-05 20:26:03 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// index.js
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const productsRoute = require("./routes/productsRoute");
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT;
+
+connectDB();
+
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Backend API");
+});
+app.use("/api", productoRoute);
+app.use("/api", clienteRoute);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
